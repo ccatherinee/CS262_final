876ad00fc60eb05481cad48d9427906541cf279c
ccatherinee <ccui@college.harvard.edu>
2023-04-27 13:11:55 -0400

    non fault tolerant ver, revised

--- constants.py
+++ constants.py
@@ -1,22 +1,9 @@
-WORKER_PORT_START = 12435
+WORKER_PORT_START = 12244
 
-# LB -> WORKER
 MAP = 0 
-SHUFFLE = 4
-REDUCE = 12
+REDUCE = 4
 
-# worker -> LB
-MAP_CONFIRM = MAP + 1
-SHUFFLE_CONFIRM = SHUFFLE + 1 
-REDUCE_CONFIRM = REDUCE + 1
-SHUFFLE_ERROR = SHUFFLE + 2
-
-# LB -> worker
-SHUFFLE_UPDATE = SHUFFLE + 3
-SHUFFLE_STATE_REQUEST = SHUFFLE + 4
-
-# if you encounter a situation in which you're unable to send something to a fellow worker socket, then send the lb a shuffle_error with the socket you tried to sent to and failed 
-# socket gets SHUFFLE_ERROR, failed_socket_port and replaces the failed_socket_port with a new_socket_port and updates all the worker nodes SHUFFLE_UPDATE failed_port new_port 
-# upon received SHUFFLE_UPDATE, worker nodes should just replce the failed_port with new_port and resend 
-
-# no need for SHUFFLE_ERROR actually, if worker encounters an error whil
+MAP_CONFIRM = MAP + 1 
+REDUCE_CONFIRM = REDUCE + 1 
+REQUEST = REDUCE + 2 
+GIVE = REDUCE + 3
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class MRWordFreqCount(MRJob):
         yield k, sum(v)
 if __name__ == '__main__':
     blah = MRWordFreqCount()
-    blah.run([(0, "the a the"), (1, "four a the")])
+    blah.run([(0, "the a the cat in the hat something okay whatever"), (1, "four a the something is up cat is cool hat cat cat")])
 
 # the [1, 1, 1] cat[1, 1] a[1, 1]
 # that[1]
