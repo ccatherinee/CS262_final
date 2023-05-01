253c22359704b696da0f6a229bd82147c000dc31
ccatherinee <ccui@college.harvard.edu>
2023-05-01 18:03:17 -0400

    this is broken

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class MRWordFreqCount(MRJob):
         yield k, sum(v)
 if __name__ == '__main__':
     blah = MRWordFreqCount()
-    blah.run([(0, "the a the cat in the hat something okay whatever"), (1, "four a the something is up cat is cool hat cat cat")])
+    blah.run([(0, "cat the four four"), (1, "four cat")])
 
 # the [1, 1, 1] cat[1, 1] a[1, 1]
 # that[1]
