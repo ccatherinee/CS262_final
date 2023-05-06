3783e826d2fac00c2770866cd21bb03d5401cbba
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2023-05-06 10:17:18 -0400

    Change example input files

--- new/user.py
+++ new/user.py
@@ -4,7 +4,7 @@ from master import MRJob
 # Example MapReduce job for counting word frequencies in some documents mr-input-1.txt through mr-input-4.txt
 class MRWordFreqCount(MRJob):
     def mapper(self, _, line):
-        for word in line.split(' '):
+        for word in line.split():
             yield word.lower(), 1
             
     def reducer(self, k, v): 
