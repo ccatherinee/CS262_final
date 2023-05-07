595609fb1188b6e12f32d6b1dbb543aa9a46b540
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2023-05-07 11:22:39 -0400

    Change big example to be bigger

---
+++ new/example_2_big/user.py
@@ -0,0 +1,18 @@
+from master import MRJob
+from constants import *
+
+# Example MapReduce job for word counting in some (large, complicated) documents mr-input-1.txt through mr-input-R.txt
+class MRWordFreqCount(MRJob):
+    def mapper(self, _, line):
+        # remove punctuation and make lowercase
+        line = line.translate(str.maketrans('', '', punctuation))
+        for word in line.split():
+            yield word.lower(), 1
+            
+    def reducer(self, k, v): 
+        yield sum(v)
+
+
+if __name__ == '__main__':
+    MRWordFreqCount(M=10, R=3).run() # 10 map tasks, 3 reduce tasks
+    print("User's MapReduce job completed! User program can continue now :)")
