e1d33750cae2344e39de93b639e79adb104a07be
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2023-05-07 14:12:16 -0400

    Add 25 big text input files

--- new/user.py
+++ new/user.py
@@ -1,5 +1,6 @@
 from master import MRJob
 from constants import *
+import time
 
 # Example MapReduce job for word counting in some (large, complicated) documents mr-input-1.txt through mr-input-R.txt
 class MRWordFreqCount(MRJob):
@@ -7,12 +8,14 @@ class MRWordFreqCount(MRJob):
         # remove punctuation and make lowercase
         line = line.translate(str.maketrans('', '', punctuation))
         for word in line.split():
-            yield word.lower().strip(), 1
+            yield word.lower(), 1
             
     def reducer(self, k, v): 
         yield sum(v)
 
 
 if __name__ == '__main__':
-    MRWordFreqCount(M=10, R=3).run() # 10 map tasks, 3 reduce tasks
+    starttime = time.time()
+    MRWordFreqCount(M=25, R=5).run()
     print("User's MapReduce job completed! User program can continue now :)")
+    print(f"Took {time.time - starttime} seconds to finish!")
