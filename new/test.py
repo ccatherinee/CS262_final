49bca98285270c042d53a3a161dc5e1273137b69
ccatherinee <70657792+ccatherinee@users.noreply.github.com>
2023-05-08 11:34:34 -0400

    finish testing

--- new/test.py
+++ new/test.py
@@ -1,60 +1,265 @@
+import unittest
 from unittest import mock 
 from unittest.mock import patch, call, ANY
 import socket 
 import selectors 
 import user 
 import master
+import worker
 import constants 
 import struct 
 import queue 
 import types 
-import unittest 
-import worker
+import pathlib
 
+class TestWorker(unittest.TestCase):
 
-class TestWorker(unittest.TestCase): 
-    
-    @mock.patch("builtins.print")
-    @mock.patch("selectors.DefaultSelector")
+    @mock.patch("builtins.open")
+    @mock.patch("json.loads")
     @mock.patch("socket.socket")
-    def test_accept_worker_connection(self, mock_socket, mock_selector, mock_print): 
-        worker_1 = worker.Worker(testing=True)
+    @mock.patch("selectors.DefaultSelector")
+    def test_reduce(self, mock_selector, mock_socket, mock_loads, mock_open):
+        self.worker_node = worker.Worker(testing=True)
+        self.assertEqual(self.worker_node.write_to_master_queue.qsize(), 1)
+        self.worker_node.M = 2
+        self.worker_node.R = 2
+        self.worker_node.reduce_task = 2
+        self.worker_node.request_intermediate_from = queue.Queue()
+        self.worker_node.request_intermediate_from.put((1,'host1',1234))
+        self.worker_node.request_intermediate_from.put((2,'host2',4321))
+
+        self.worker_node.intermediate_results = queue.Queue()
+        self.worker_node.intermediate_results.put((1,{}))
+        self.worker_node.intermediate_results.put((2,{}))
+
+        def f(a,b,c): yield sum(c)
+        self.worker_node.reducer = f 
+        mock_loads.side_effect = [[('hello',1), ('my',1),('name',1),('is',1),('hello',1)], [('yes',1), ('my',1), ('name',1),('is',1)]]
+        self.worker_node.reduce_thread()
+
+        mock_open.assert_called_once_with('mr-output-2.txt','w')
+        mock_open().__enter__().write.assert_has_calls([call("hello\t2\n"), call("my\t2\n"),call("name\t2\n"),call("is\t2\n"),call("yes\t1\n")])
+        self.assertEqual(self.worker_node.request_intermediate_from.empty(), True)
+        mock_socket.return_value.connect.assert_has_calls([call(('host1',1234)), call(('host2',4321))])
+        mock_selector.return_value.register.assert_has_calls([call(ANY,1,data=None), call(ANY,2,data=ANY)])
+        self.assertEqual(self.worker_node.intermediate_results.empty(), True)
+        self.assertEqual(self.worker_node.write_to_master_queue.qsize(), 3)
+        self.assertEqual(self.worker_node.reduce_task, None)
+
+    @mock.patch("builtins.hash")
+    @mock.patch("json.dump")
+    @mock.patch("builtins.open")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_map(self, mock_sel, mock_sock, mock_open, mock_dump, mock_hash):
+        self.worker_node = worker.Worker(testing=True)
+        self.assertEqual(self.worker_node.write_to_master_queue.qsize(), 1)
+        self.worker_node.M = 2
+        self.worker_node.R = 2
+        self.worker_node.map_task = 2
+
+        def f(a,b,c):
+            import string
+            line = c.translate(str.maketrans('', '', string.punctuation))
+            for word in line.split():
+                yield word.lower(), 1
+
+        mock_hash.side_effect = [1,2,2,1,2,1,1,2,2]
+                
+        self.worker_node.mapper = f 
+        self.worker_node.map_thread("hello my name is\n yes that is my name")
+        mock_hash.assert_has_calls([call('hello'), call('my'), call('name'), call('is'), call('yes'), call('that'), call('is'), call('my'), call('name')], any_order=True)
+        mock_open.assert_has_calls([call('mr-2-1.json','w'), call('mr-2-2.json','w')], any_order=True)
+        mock_dump.assert_has_calls([call([('my', 1), ('name', 1), ('yes', 1), ('my', 1), ('name', 1)], ANY), call([('hello', 1), ('is', 1), ('that', 1), ('is', 1)], ANY)])
+        self.assertEqual(self.worker_node.write_to_master_queue.qsize(),3)
+        self.assertEqual(self.worker_node.map_task, None)
+
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_accept_worker_connection(self, mock_sel, mock_sock):
+        self.worker_node = worker.Worker(testing=True)
         mock_conn = mock.Mock(name="conn")
-        mock_socket.return_value.accept.return_value = (mock_conn, ("hostasdf", 1234))
-        worker_1.accept_worker_connection()
-        mock_conn.setblocking.assert_called_once_with(False)
+        mock_sock.return_value.accept.return_value = (mock_conn, ("hostasdf",1234))
+        self.worker_node.accept_worker_connection()
+        mock_sel.return_value.register.assert_has_calls([call(mock_conn, selectors.EVENT_READ, data=ANY), call(mock_conn, selectors.EVENT_WRITE,data=ANY)])
 
-        read_sel = mock_selector.return_value 
-        write_sel = mock_selector.return_value 
-        write_sel.register.assert_called_with(mock_conn, selectors.EVENT_WRITE, data=types.SimpleNamespace(addr=("hostasdf", 1234), write_to_worker_queue=ANY))
-        # read_sel.register.assert_called_with(mock_conn, selectors.EVENT_READ, data=types.SimpleNamespace(addr=("hostasdf", 1234), write_to_worker_queue=ANY))
-    
-
-    @mock.patch("socket.socket")
+    @mock.patch("worker.Worker._recvall")
     @mock.patch("struct.unpack")
-    @mock.patch("worker.Worker")
+    @mock.patch("socket.socket")
     @mock.patch("selectors.DefaultSelector")
-    def test_service_master_connection(self, mock_selector, mock_worker, mock_unpack, mock_socket): 
+    def test_service_master_complete(self, mock_sel, mock_sock, mock_unpack, mock_recvall):
+        self.worker_node = worker.Worker(testing=True)
         mock_key = mock.Mock(name="key")
-        mock_sock = mock.Mock(name="sock")
-        mock_key.fileobj = mock_sock
 
-        mock_master = mock_socket.return_value
-        mock_worker.master_sock = mock_master
         mock_unpack.return_value = (constants.ALL_TASKS_COMPLETE,)
-
-        mock_worker.service_master_connection(mock_key, selectors.EVENT_READ)
-        mock_selector.return_value.unregister.assert_called_with(mock_worker.master_sock)
-        # mock_worker.master_sock.return_value.close.assert_called_once()
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        mock_sel.return_value.unregister.assert_called_with(self.worker_node.master_sock)
+        self.worker_node.master_sock.close.assert_called_once()
+        self.assertEqual(status, True)
         
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_down(self, mock_sel, mock_sock, mock_unpack, mock_recvall):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key = mock.Mock(name="key")
+        
+        #master not up, so worker should abort
+        mock_recvall.return_value = None
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        mock_sel.return_value.unregister.assert_called_with(self.worker_node.master_sock)
+        self.worker_node.master_sock.close.assert_called_once()
+        self.assertEqual(status, True)
 
+    @mock.patch("time.sleep")
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_no_avail(self, mock_sel, mock_sock, mock_unpack, mock_recvall, mock_sleep):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key = mock.Mock(name="key")
+        mock_unpack.return_value = (constants.NO_AVAILABLE_TASK,)
+        start_bytes = self.worker_node.bytes_sent
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        mock_sleep.assert_called_once_with(1)
+        self.worker_node.master_sock.sendall.assert_called_once_with(struct.pack('>Q',constants.REQUEST_TASK))
+        self.assertEqual(self.worker_node.bytes_sent - start_bytes, 8)
+        self.assertEqual(status, False)
 
-class TestMRJob(unittest.TestCase): 
+    @mock.patch("threading.Thread")
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_map(self, mock_sel, mock_sock, mock_unpack, mock_recvall, mock_thread):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key = mock.Mock(name="key")
+        mock_unpack.side_effect = [(constants.MAP_TASK,), (2,), (2,), (2,), (100,), (1000,)]
+        mock_recvall.return_value.decode.side_effect = ["def mapper(a,b,c): return b", "hello this is the input"]
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        self.assertEqual(self.worker_node.map_task, 2)
+        self.assertEqual(self.worker_node.M, 2)
+        self.assertEqual(self.worker_node.R, 2)
+        self.assertEqual(self.worker_node.mapper(1,2,3),2)
+        mock_thread.assert_called_once_with(target=self.worker_node.map_thread, args=("hello this is the input",))
+        mock_thread.return_value.start.assert_called_once()
+        self.assertEqual(status, False)
+
+    @mock.patch("threading.Thread")
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_reduce(self, mock_sel, mock_sock, mock_unpack, mock_recvall, mock_thread):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key = mock.Mock(name="key")
+        mock_unpack.side_effect = [(constants.REDUCE_TASK,), (1,), (2,), (2,), (100,), (1000,)]
+        mock_recvall.return_value.decode.return_value = "def reducer(a,b,c): return sum(c)"
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        self.assertEqual(self.worker_node.reduce_task, 1)
+        self.assertEqual(self.worker_node.M, 2)
+        self.assertEqual(self.worker_node.R, 2)
+        self.assertEqual(self.worker_node.reducer(1,2,[1,1,1,1,2]),6)
+        mock_thread.assert_called_once_with(target=self.worker_node.reduce_thread)
+        mock_thread.return_value.start.assert_called_once()
+        self.assertEqual(status, False)
+        
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_reduce_location(self, mock_sel, mock_sock, mock_unpack, mock_recvall):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key = mock.Mock(name="key")
+        mock_unpack.side_effect = [(constants.REDUCE_LOCATION_INFO,), (20,), (8,), (55555,)]
+        mock_recvall.return_value.decode.return_value = "hostasdf"
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        self.assertEqual(self.worker_node.request_intermediate_from.get(), (20, "hostasdf",55555))
+        self.assertEqual(status, False)
+
     @mock.patch("builtins.print")
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_invalid_op(self, mock_sel, mock_sock, mock_unpack, mock_recvall, mock_print):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key = mock.Mock(name="key")
+        mock_unpack.return_value = "INVALID_OPCODE"
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_READ)
+        mock_print.assert_called_with("ERROR: Invalid opcode received from master node")
+
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_master_write(self, mock_sel, mock_sock, mock_unpack, mock_recvall):
+        self.worker_node = worker.Worker(testing=True)
+        self.assertEqual(self.worker_node.write_to_master_queue.get(), b'\x00\x00\x00\x00\x00\x00\x00\x01')
+        self.worker_node.write_to_master_queue.put(b'\x00\x00\x00\x00\x00\x00\x00\x01')
+        self.worker_node.write_to_master_queue.put(b'\x00\x00\x00\x00\x00\x00\x00\x02')
+        self.worker_node.write_to_master_queue.put(b'\x00\x00\x00\x00\x00\x00\x00\x03')
+        mock_key = mock.Mock(name="key")
+        status = self.worker_node.service_master_connection(mock_key, selectors.EVENT_WRITE)
+        self.assertEqual(self.worker_node.write_to_master_queue.empty(), True)
+        self.worker_node.master_sock.sendall.assert_has_calls([call(b'\x00\x00\x00\x00\x00\x00\x00\x01'), call(b'\x00\x00\x00\x00\x00\x00\x00\x02'), call(b'\x00\x00\x00\x00\x00\x00\x00\x03')])
+        self.assertEqual(self.worker_node.bytes_sent, len(b'\x00\x00\x00\x00\x00\x00\x00\x01')*3)
+        self.assertEqual(status, False)
+        
+    @mock.patch("builtins.open")
+    @mock.patch("worker.Worker._recvall")
+    @mock.patch("struct.unpack")
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_worker_read(self, mock_sel, mock_sock, mock_unpack, mock_recvall, mock_open):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key_1 = mock.Mock(name="key1")
+        mock_sock_1 = mock.Mock(name="sock1")
+        mock_key_1.fileobj = mock_sock_1
+        mock_key_1.data.write_to_worker_queue = queue.Queue()
+
+        #worker requests map results
+        mock_unpack.side_effect = [(constants.MAP_RESULTS_REQUEST,), (1,), (1,)]
+        mock_open.return_value.read.return_value = struct.pack('>Q', constants.MAP_RESULTS_REQUEST)
+        self.worker_node.service_worker_read(mock_key_1, selectors.EVENT_READ)
+        mock_open.assert_called_with("mr-1-1.json",'rb')
+        self.assertEqual(mock_key_1.data.write_to_worker_queue.qsize(), 1)
+
+        #worker sends map results
+        mock_unpack.side_effect = [(constants.MAP_RESULTS,), (1,), (1000,)]
+        mock_recvall.return_value = "FILLER MAP RESULTS"
+        self.worker_node.service_worker_read(mock_key_1, selectors.EVENT_READ)
+        self.assertEqual(self.worker_node.intermediate_results.get(), (1,"FILLER MAP RESULTS"))
+
+        #worker disconnects
+        mock_recvall.return_value = None
+        self.worker_node.service_worker_read(mock_key_1, selectors.EVENT_READ)
+        mock_sel.return_value.unregister.assert_called_with(mock_sock_1)
+        mock_sock_1.close.assert_called_once()
+        
+    @mock.patch("socket.socket")
+    @mock.patch("selectors.DefaultSelector")
+    def test_service_worker_write(self, mock_sel, mock_sock):
+        self.worker_node = worker.Worker(testing=True)
+        mock_key_1 = mock.Mock(name="key1")
+        mock_sock_1 = mock.Mock(name="sock1")
+        mock_key_1.fileobj = mock_sock_1
+        mock_key_1.data.write_to_worker_queue = queue.Queue()
+        test_msg = struct.pack('>Q', constants.MAP_RESULTS_REQUEST)
+        mock_key_1.data.write_to_worker_queue.put(test_msg)
+        
+        self.worker_node.service_worker_write(mock_key_1, selectors.EVENT_WRITE)
+        self.assertEqual(mock_key_1.data.write_to_worker_queue.empty(), True)
+        mock_sock_1.sendall.assert_called_with(test_msg)
+        self.assertEqual(self.worker_node.bytes_sent, len(test_msg))
+        
+class TestMRJob(unittest.TestCase):
     @mock.patch("time.sleep")
     @mock.patch("selectors.DefaultSelector")
     @mock.patch("socket.socket")
-    def test_run(self, mock_socket, mock_selector, mock_sleep, mock_print): 
+    def test_run(self, mock_socket, mock_selector, mock_sleep): 
         self.master_node = master.MRJob(4, 2)
         self.master_node.run(True)
         mock_socket.return_value.bind.assert_called_once_with((constants.MASTER_HOST, constants.MASTER_PORT))
@@ -76,6 +281,11 @@ class TestMRJob(unittest.TestCase):
         mock_selector.return_value.register.assert_called_with(mock_conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=types.SimpleNamespace(write_to_worker_queue=ANY, worker_addr=("hostasdf", 1234)))
         self.assertEqual(self.master_node.worker_connections, {("hostasdf", 1234): mock_conn})
 
+    """
+    For testing, we chose to write the service_worker_connection test as one large test because the setup between steps would be excessive otherwise. 
+    This is because in addition to testing the individual operations, we also want to test that the state is maintained correctly between steps,
+    so it makes sense to test the service_worker_connection function holistically as if it were operating on an actual MapReduce job
+    """
     @mock.patch("selectors.DefaultSelector")
     @mock.patch("struct.unpack")
     @mock.patch("master.MRJob._recvall")
@@ -284,6 +494,5 @@ class TestMRJob(unittest.TestCase):
         ans = self.master_node._recvall(mock_sock, 10)
         self.assertEqual(ans, b"abcdefghij")
 
-        
 if __name__ == '__main__':
     unittest.main()
